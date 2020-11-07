b481a5b6afd444fa5741eb376dc38b744fa7f589
Crazy-White <565332016@qq.com>
2020-11-07 17:22:17 +0800

    toFix

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # alerting.js
+
 A slight,optional alert function based on promise.
 Inspired by SweetAlert, for any normal situation, strongly recommend.
+
 # Usage
+
 ```html
-    <script src="./alerting.js"></script>
-    <link href="./alerting.css" rel="stylesheet">
+<script src="./alerting.js"></script>
+<link href="./alerting.css" rel="stylesheet" />
 ```
+
 with cdn
+
 ```html
-    <script src="https://cdn.jsdelivr.net/gh/Crazy-White/alerting.js/alerting.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/gh/Crazy-White/alerting.js/alerting.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/Crazy-White/alerting.js/alerting.min.js"></script>
+<link href="https://cdn.jsdelivr.net/gh/Crazy-White/alerting.js/alerting.min.css" rel="stylesheet" />
 ```
--- alerting.js
+++ alerting.js
@@ -1,123 +1,108 @@
-
 function alerting(options, callback) {
-
     return new Promise((resolve, reject) => {
-
         let buttons, content;
-        let container = document.createElement("div")
-        container.className = "alerting-container"
-        let mask = document.createElement("div")
-        mask.className = "alerting-mask"
-        let modal = document.createElement("div")
-        modal.className = "alerting-modal"
+        let container = document.createElement('div');
+        container.className = 'alerting-container';
+        let mask = document.createElement('div');
+        mask.className = 'alerting-mask';
+        let modal = document.createElement('div');
+        modal.className = 'alerting-modal';
 
-
-        if (typeof options == "object") {
-
-            if ("timeout" in options) {
+        if (typeof options == 'object') {
+            if ('timeout' in options) {
                 if (!isNaN(Number(options.timeout))) {
-                    setTimeout(() => waitClose().then(() => resolve(null)), Number(options.timeout))
+                    setTimeout(() => waitClose().then(() => resolve(null)), Number(options.timeout));
                 }
             }
 
-            if ("style" in options) {
-                modal.style.cssText = options.style.modal
-                container.style.cssText = options.style.container
+            if ('style' in options) {
+                modal.style.cssText = options.style.modal;
+                container.style.cssText = options.style.container;
             }
 
-
-            if ("elements" in options) {
-                let elements = options.elements
+            if ('elements' in options) {
+                let elements = options.elements;
 
                 for (name in elements) {
-
-                    if (name == "close") {
-                        let close = document.createElement("div")
-                        close.innerHTML = '✖'
-                        close.className = 'alerting-close'
-                        close.onclick = () => waitClose().then(() => resolve(null))
-                        container.prepend(close)
-                        continue
+                    if (name == 'close') {
+                        let close = document.createElement('div');
+                        close.innerHTML = '✖';
+                        close.className = 'alerting-close';
+                        close.onclick = () => waitClose().then(() => resolve(null));
+                        container.prepend(close);
+                        continue;
                     }
 
-                    let ele = typeof elements[name] == "function" ? elements[name]() : elements[name]
-                    container.append(ele)
+                    let ele = typeof elements[name] == 'function' ? elements[name]() : elements[name];
+                    container.append(ele);
                 }
             }
 
-
-            if ("buttons" in options) {
-                let buttons = options.buttons
+            if ('buttons' in options) {
+                let buttons = options.buttons;
 
                 for (name in buttons) {
-                    let $btn = document.createElement("button")
-                    let btn = buttons[name]
-                    $btn.innerHTML = btn.content ? btn.content : 'button'
+                    let $btn = document.createElement('button');
+                    let btn = buttons[name];
+                    $btn.innerHTML = btn.content ? btn.content : 'button';
 
-                    if (name == "confirm") {
-                        $btn.className += " alerting-confirm"
-                        $btn.innerHTML = btn.content ? btn.content : `Confirm`
+                    if (name == 'confirm') {
+                        $btn.className += ' alerting-confirm';
+                        $btn.innerHTML = btn.content ? btn.content : `Confirm`;
                     }
 
-                    if (name == "cancel") {
-                        $btn.className += " alerting-cancel"
-                        $btn.innerHTML = btn.content ? btn.content : `Cancel`
+                    if (name == 'cancel') {
+                        $btn.className += ' alerting-cancel';
+                        $btn.innerHTML = btn.content ? btn.content : `Cancel`;
                     }
 
-                    btn.style ? $btn.style.cssText = btn.style : ""
-                    btn.class ? $btn.className += ' ' + btn.class : "";
+                    btn.style ? ($btn.style.cssText = btn.style) : '';
+                    btn.class ? ($btn.className += ' ' + btn.class) : '';
                     $btn.addEventListener('click', () => {
-                        if (typeof btn.data == "undefined") {
-                            btn.data = (name == "confirm") ? true : ((name == "cancel") ? false : '')
+                        if (typeof btn.data == 'undefined') {
+                            btn.data = name == 'confirm' ? true : name == 'cancel' ? false : '';
                         } else {
-                            btn.data = (typeof btn.data == "function") ? btn.data() : btn.data
+                            btn.data = typeof btn.data == 'function' ? btn.data() : btn.data;
                         }
 
-                        if ("close" in btn && btn.close == false) {
-                            resolve(btn.data)
+                        if ('close' in btn && btn.close == false) {
+                            resolve(btn.data);
                         } else {
-                            waitClose().then(() => resolve(btn.data))
+                            waitClose().then(() => resolve(btn.data));
                         }
-                    })
-                    container.append($btn)
+                    });
+                    container.append($btn);
                 }
             }
 
-
-            content = options.content
+            content = options.content;
         } else {
-            modal.className += " alerting-notObject-modal"
-            container.className += " alerting-notObject-container"
-            content = options.toString()
+            modal.className += ' alerting-notObject-modal';
+            container.className += ' alerting-notObject-container';
+            content = options.toString();
         } // end if
 
-
-        container.insertAdjacentHTML('afterbegin', content)
-        modal.prepend(container)
-        callback ? callback(container) : ""
-
-
+        container.insertAdjacentHTML('afterbegin', content);
+        modal.prepend(container);
+        callback ? callback(container) : '';
 
         function waitClose() {
             return new Promise((resolve, reject) => {
-                modal.className += " alerting-fadeOut"
-                mask.className += " alerting-fadeOut"
-                modal.addEventListener("animationend", () => {
-                    modal.remove()
-                    mask.remove()
-                    resolve()
+                modal.className += ' alerting-fadeOut';
+                mask.className += ' alerting-fadeOut';
+                modal.addEventListener('animationend', () => {
+                    modal.remove();
+                    mask.remove();
+                    resolve();
                 });
-            })
+            });
         } // end function
 
-
-
         mask.addEventListener('click', () => {
-            console.debug("clicked mask")
-            waitClose().then(() => resolve(null))
-        })
+            //console.debug('clicked mask');
+            waitClose().then(() => resolve(null));
+        });
 
-        document.body.append(mask, modal)
-
-    })
+        document.body.append(mask, modal);
+    });
 }
